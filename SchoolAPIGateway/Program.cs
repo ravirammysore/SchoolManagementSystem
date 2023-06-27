385a4ba8c56e8ca66796b33fb61a03968a1ca255
Ravi K R <raviram.mysore@gmail.com>
2023-06-27 22:24:19 +0530

    Further simplified the application by removing application settings file in the services and also using memorable port numbers

--- SchoolAPIGateway/Program.cs
+++ SchoolAPIGateway/Program.cs
@@ -2,25 +2,11 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseOcelot();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
-
 app.Run();
